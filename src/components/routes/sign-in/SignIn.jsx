a9b4abb4d480217499b634ff55eff740c79f82c1
Crims91 <cucerst@gmail.com>
2023-02-02 13:32:36 +0200

    created SingUp, FormInput, Button components

--- src/components/routes/sign-in/SignIn.jsx
+++ src/components/routes/sign-in/SignIn.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import {
   signInWithGooglePopUp,
   createUserDocumentFromAuth,
 } from "../../../utils/firebase/firebase.utils";
+import SignUpForm from "../../signUpForm/SignUpForm";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
@@ -13,6 +15,7 @@ const SignIn = () => {
     <div>
       <h1>Sign In</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUpForm />
     </div>
   );
 };
